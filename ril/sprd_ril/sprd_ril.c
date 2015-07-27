3e4d121f8619bb98508911c7fb5bf135bf7004e2
yinpan.fu <yinpan.fu@spreadtrum.com>
2015-07-27 15:08:36 +0800

    Bug #458678 ps not detach after AP send closedata, but AP set sLteRegState to State_Outof_service, and can not send pdp activate
    [bug number  ] 458678
    [root cause  ] ps not detach after AP send closedata, but AP set
    sLteRegState to State_Outof_service, and can not send pdp activate
    [changes     ] not set sLteRegState to State_Outof_service when AP send
    closedata,this state depends on AT+CEREG?
    [side effects] no
    [self test   ] <board:5.1_trunk><test case:switchdata card and primary
    card 20 times, send mms>
    [whether AOB ] no
    [reviewers   ] lina.liu
    
    Change-Id: I0fe19d1c2632495a3c55d381c6f70ce2864f1447

--- ril/sprd_ril/sprd_ril.c
+++ ril/sprd_ril/sprd_ril.c
@@ -2088,7 +2088,7 @@ static void requestRadioPower(int channelID, void *data, size_t datalen, RIL_Tok
                 putPDP(i);
             }
         }
-        if (isLte()) {
+        if (!strcmp(s_modem, "l") && isSvLte()) {
             pthread_mutex_lock(&s_lte_attach_mutex);
             sLteRegState = STATE_OUT_OF_SERVICE;
             pthread_mutex_unlock(&s_lte_attach_mutex);
@@ -11156,12 +11156,6 @@ static void detachGPRS(int channelID, void *data, size_t datalen, RIL_Token t)
         if (s_testmode != 10) {
             at_send_command(ATch_type[channelID], "AT+CLSSPDT = 1", NULL);
         }
-        pthread_mutex_lock(&s_lte_attach_mutex);
-        if(sLteRegState == STATE_IN_SERVICE){
-            sLteRegState = STATE_OUT_OF_SERVICE;
-        }
-        pthread_mutex_unlock(&s_lte_attach_mutex);
-        RILLOGD("detachGPRS set sLteRegState: OUT OF SERVICE.");
     }else{
         err = at_send_command(ATch_type[channelID], "AT+SGFD", &p_response);
     }
