0433c1e8210d0df7f07ee4ed9dd186848cbb6b46
duan xue <duan.xue@spreadtrum.com>
2015-08-07 13:13:39 +0800

    Bug #461142 update mpeg4 software decoder to support bitstream without vol header
    [root cause  ] 461142
    [changes     ] update mpeg4 software decoder to support bitstream without vol header
    [side effects] mpeg4 software decoder
    [self test   ]<board, Test cases summary>:
    [reviewers   ] xiaowei.luo
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I3c35a2fc0f1c73f2a3a3cc759364709af3ba75c6
    (cherry picked from commit 841e8badd0ad5b6af37aadc4f332f13427c5026b)

--- sprd_codecs/openmax/m4v_h263_mix/dec/sw/src/mp4dec_interface.c
+++ sprd_codecs/openmax/m4v_h263_mix/dec/sw/src/mp4dec_interface.c
@@ -189,9 +189,18 @@ PUBLIC MMDecRet MP4DecVolHeader(MP4Handle *mp4Handle, MMDecVideoFormat *video_fo
 
             ret = Mp4Dec_DecMp4Header(vop_mode_ptr, video_format_ptr->i_extra);
             if(MMDEC_OK == ret)
-            {
-                video_format_ptr->frame_width = vop_mode_ptr->OrgFrameWidth;
-                video_format_ptr->frame_height= vop_mode_ptr->OrgFrameHeight;
+            {  //revised for bug456978
+                if (vop_mode_ptr->OrgFrameWidth != 0 && vop_mode_ptr->OrgFrameHeight != 0)
+                {
+                    video_format_ptr->frame_width = vop_mode_ptr->OrgFrameWidth;
+                    video_format_ptr->frame_height= vop_mode_ptr->OrgFrameHeight;
+                }
+                else
+                {
+                    vop_mode_ptr->OrgFrameWidth =  video_format_ptr->frame_width;
+                    vop_mode_ptr->OrgFrameHeight = video_format_ptr->frame_height;
+                }
+
 
                 vop_mode_ptr->FrameWidth =  ((vop_mode_ptr->OrgFrameWidth  + 15)>>4)<<4;
                 vop_mode_ptr->FrameHeight  = ((vop_mode_ptr->OrgFrameHeight + 15) >>4)<<4;
