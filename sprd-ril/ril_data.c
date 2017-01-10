6654e6e1175cb2dd23cfe4d62172e32fe94ed91e
cuicui.chen <cuicui.chen@spreadtrum.com>
2017-01-10 14:41:35 +0800

    Bug #632000 Only set AT+SPREATTACH on data card
    [root cause  ] set AT+SPREATTACH on multi card, lead to CP abnormal
    [changes     ] ril_data.c
    [side effects] N/A
    [self test   ]<board, Test cases summary>:<9832>, Switch primary card to SIM2 and set gsm only mode, change apn can setup datacall
    [reviewers   ] zhiwei.fu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I39e87de82e24c4c69ac5ce4590b6b15def6a877f

--- sprd-ril/ril_data.c
+++ sprd-ril/ril_data.c
@@ -1645,12 +1645,12 @@ static void requestSetInitialAttachAPN(int channelID, void *data,
             }
             RLOGD("get_data_profile s_PSRegStateDetail=%d, s_in4G=%d",
                    s_PSRegStateDetail[socket_id], s_in4G[socket_id]);
-            if (s_in4G[socket_id] == 1 ||
+            if (socket_id == s_multiModeSim && (s_in4G[socket_id] == 1 ||
                 s_PSRegStateDetail[socket_id] == RIL_REG_STATE_NOT_REG ||
                 s_PSRegStateDetail[socket_id] == RIL_REG_STATE_ROAMING ||
                 s_PSRegStateDetail[socket_id] == RIL_REG_STATE_SEARCHING ||
                 s_PSRegStateDetail[socket_id] == RIL_REG_STATE_UNKNOWN ||
-                s_PSRegStateDetail[socket_id] == RIL_REG_STATE_DENIED) {
+                s_PSRegStateDetail[socket_id] == RIL_REG_STATE_DENIED)) {
                 at_send_command(s_ATChannels[channelID], "AT+SPREATTACH", NULL);
             }
         } else {
