8c1c0e7d02cb155dad514492d6f87b4d3ec181dd
yanxue.xu <yanxue.xu@spreadtrum.com>
2017-01-12 14:47:15 +0800

    Bug#626153 stk cannot be installed after hot plug in/out sim card.
    [root cause  ] when hot plug in/out sim card fast, AP did not notify the modem the stk application is not ready,
    so the modem did not have buffer resulting stk cannot be installed
    [changes     ] reponse cp no report setup menu after stk application is ready
    [side effects] no
    [self test   ]<board, Test cases summary>:9860-3,adb push,hot plug in/out sim card fast,stk application can install successfull,pass
    [reviewers   ] kun.tang daxing.cao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I42522b9947cea6456368e59e109af6481f9251da

--- sprd-ril/ril_sim.c
+++ sprd-ril/ril_sim.c
@@ -8,6 +8,7 @@
 
 #include "sprd_ril.h"
 #include "ril_sim.h"
+#include "ril_stk.h"
 #include "ril_utils.h"
 #include "ril_network.h"
 #include "custom/ril_custom.h"
@@ -2961,11 +2962,13 @@ void onSimStatusChanged(RIL_SOCKET_ID socket_id, const char *s) {
                         s_simState[socket_id] = SIM_REMOVE;
                         RIL_requestTimedCallback(onSimAbsent,
                                 (void *)&s_socketId[socket_id], NULL);
-                    } else if (cause == 1) {  // no sim card
+                    } else if (cause == 1 || cause == 7) {  // no sim card
                         RIL_onUnsolicitedResponse(
                                 RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED, NULL, 0,
                                 socket_id);
                     }
+                    // sim hot plug out and set stk to not enable
+                    s_stkServiceRunning[socket_id] = false;
                 }
             } else if (value == 100 || value == 4) {
                 RIL_onUnsolicitedResponse(RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED,
--- sprd-ril/ril_stk.h
+++ sprd-ril/ril_stk.h
@@ -8,6 +8,8 @@
 #ifndef RIL_STK_H_
 #define RIL_STK_H_
 
+extern bool s_stkServiceRunning[SIM_COUNT];
+
 int processStkRequests(int request, void *data, size_t datalen, RIL_Token t,
         int channelID);
 int processStkUnsolicited(RIL_SOCKET_ID socket_id, const char *s);
