10849d611b1175a868075c803a9ef2d97e0aae3d
lina.liu <lina.liu@spreadtrum.com>
2017-01-13 15:31:40 +0800

    Bug #634417 close sim2, open sim2, change data card from sim1 to sim2, it is failed to active pdp on sim2
    [root cause  ] put pdp after sfun=5 on dual sim
    [changes     ] ril
    [side effects] no
    [self test   ]<board, Test cases summary>:<9832, close sim2, open sim2, change data card from sim1 to sim2, it is successful to active pdp on sim2>
    [reviewers   ]
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ib4fa45eaf262064177597d1fb8964196af2bd2c4
    (cherry picked from commit ea155441ac98c0a1286899c7c47ad9ecfd4e2db4)

--- sprd-ril/ril_network.c
+++ sprd-ril/ril_network.c
@@ -970,7 +970,7 @@ static void requestRadioPower(int channelID, void *data, size_t datalen,
         }
 
         for (i = 0; i < MAX_PDP; i++) {
-            if (s_PDP[i].state == PDP_BUSY) {
+            if (s_dataAllowed[socket_id] && s_PDP[i].state == PDP_BUSY) {
                 RLOGD("s_PDP[%d].state = %d", i, s_PDP[i].state);
                 putPDP(i);
             }
