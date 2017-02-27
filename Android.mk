018a2acb5c159c1af9468a7302547fcbe5554aae
Lisayanan Li (李亚男) <SPREADTRUM\lisayanan.li@tj04992pcu.spreadtrum.com>
2017-02-27 16:17:40 +0800

    Bug #12346
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ]<board, Test cases summary>:
    [reviewers   ]

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# lisa
 
 common_ril_dirs := libril_tele libril sprd-ril rild libatci
 include $(call all-named-subdir-makefiles,$(common_ril_dirs))
