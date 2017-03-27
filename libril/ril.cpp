82c7f2ed43a2ff32d4b3b807278e97a5aa32307e
yinpan.fu <yinpan.fu@spreadtrum.com>
2017-03-27 15:10:22 +0800

    Bug #751655 for customer ss business fail
    [root cause  ] add some operators use vowifi for ss
    [changes     ] add plmn for ss use vowifi
    [side effects]no
    [self test   ]:ss business is ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:7.0_trunk
    [test case]:supplement service is ok
    [reviewers   ] zhiwei.fu
    [change_type ] bugfix
    [tag_product ] common
    
    Change-Id: I8e03fb5526f65c0cd29576432637572daa85379e

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -6650,6 +6650,9 @@ const char *requestToString(int request) {
         case RIL_EXT_REQUEST_QUERY_COLP: return "QUERY_COLP";
         case RIL_EXT_REQUEST_QUERY_COLR: return "QUERY_COLR";
         case RIL_EXT_REQUEST_MMI_ENTER_SIM: return "MMI_ENTER_SIM";
+        case RIL_EXT_REQUEST_UPDATE_OPERATOR_NAME: return "UPDATE_OPERATOR_NAME";
+        case RIL_EXT_REQUEST_SIMMGR_GET_SIM_STATUS: return "SIMMGR_GET_SIM_STATUS";
+        case RIL_EXT_REQUEST_SET_XCAP_IP_ADDR: return "SET_XCAP_IP_ADDR";
         /* }@ */
 
         case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: return "UNSOL_RESPONSE_RADIO_STATE_CHANGED";
