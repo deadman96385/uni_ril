e6e456e7db9ae7f3b3549a3b9529ad4a4fab6297
lisayanan.li <lisayanan.li@spreadtrum.com>
2017-05-04 13:50:31 +0800

    Bug #671964 [7.0_rls1] add vowifi pcscf request in ril
    [root cause  ] new feature
    [changes     ] add vowifi pcscf request in ril
    [side effects]no
    [self test   ]<board, Test cases summary>:<9832><build successfully>
    [reviewers   ]zhiwei.fu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ie89eccdf4cce7efc519395c51cb4421aa0a731a5

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -6691,6 +6691,7 @@ const char *requestToString(int request) {
         case RIL_REQUEST_IMS_NOTIFY_HANDOVER_CALL_INFO: return "RIL_REQUEST_IMS_NOTIFY_HANDOVER_CALL_INFO";
         case RIL_REQUEST_GET_IMS_SRVCC_CAPBILITY: return "RIL_REQUEST_GET_IMS_SRVCC_CAPBILITY";
         case RIL_REQUEST_GET_IMS_PCSCF_ADDR: return "GET_IMS_PCSCF_ADDR";
+        case RIL_REQUEST_SET_VOWIFI_PCSCF_ADDR: return "SET_VOWIFI_PCSCF_ADDR";
         /* }@ */
         /* OEM SOCKET REQUEST @{*/
         /* videophone @{ */
