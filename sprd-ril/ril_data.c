83ca57e58a83aafe8622ef72a7bb8a68d15adc5a
cuicui.chen <cuicui.chen@spreadtrum.com>
2017-05-17 16:22:50 +0800

    Bug #674479 Native crash when APN username is null
    [root cause  ] APN username or password is null
    [changes     ] ril_data.c
    [side effects] N/A
    [self test   ]<board, Test cases summary>:<9832, set apn username is null and boot>
    [reviewers   ] zhiwei.fu
    [change_type ] debug_log
    [tag_product ] common
    
    Change-Id: I40b1c7ec551f75d439edfcccc1d6a330fca19359
    (cherry picked from commit e5b2d9603c7c9cc6e2f44cd733fd829e9db9e6e9)

--- sprd-ril/ril_data.c
+++ sprd-ril/ril_data.c
@@ -1638,15 +1638,23 @@ static int compareApnProfile(RIL_InitialAttachApn *new,
     if (isStrEmpty(new->username) || isStrEmpty(new->password) ||
         new->authtype <= 0) {
         new->authtype = AUTH_NONE;
-        memset(new->username, 0, strlen(new->username));
-        memset(new->password, 0, strlen(new->password));
+        if (new->username != NULL) {
+            memset(new->username, 0, strlen(new->username));
+        }
+        if (new->password != NULL) {
+            memset(new->password, 0, strlen(new->password));
+        }
     }
     if (isStrEmpty(old->username) || isStrEmpty(old->password) ||
         old->authtype <= 0) {
         RLOGD("old profile is empty");
         old->authtype = AUTH_NONE;
-        memset(old->username, 0, strlen(old->username));
-        memset(old->password, 0, strlen(old->password));
+        if (old->username != NULL) {
+            memset(old->username, 0, strlen(old->username));
+        }
+        if (old->password != NULL) {
+            memset(old->password, 0, strlen(old->password));
+        }
     }
     if (isApnEqual(new->apn, old->apn) &&
         isProtocolEqual(new->protocol, old->protocol) &&
