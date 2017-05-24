52abac42882c4f3fd0f8dfa4506b35828af746a1
lisayanan.li <lisayanan.li@spreadtrum.com>
2017-05-24 09:03:28 +0800

    Bug #678953 [7.0_17a_rls1] convert gsm7bits code in +CUSD to utf8
    [root cause  ] +CUSD code encode type is gsm7bits, but decode type is for unicode now
    [changes     ] convert gsm7bits code in +CUSD to utf8
    [side effects]no
    [self test ]:build successfully, check log
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9832
    [test case]:run ussd in Hungary
    [reviewers ] zhiwei.fu [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I08ce61e889c844f3d938ab51715428f7d4ad1f91
    (cherry picked from commit 382b3226102e0425e18c6388e8022329a995ddd7)
    (cherry picked from commit 48376d7fccf69a27a3c05b0c7ed4b32a6430d528)

--- sprd-ril/ril_utils.h
+++ sprd-ril/ril_utils.h
@@ -10,5 +10,6 @@
 void convertBinToHex(char *bin_ptr, int length, unsigned char *hex_ptr);
 int convertHexToBin(const char *hex_ptr, int length, char *bin_ptr);
 void convertUcs2ToUtf8(unsigned char *ucs2, int len, unsigned char *buf);
+void convertGsm7ToUtf8(unsigned char *gsm7bits, int len, unsigned char *utf8);
 
 #endif  // RIL_UTILS_H_
