1b529aff9fc630ee9b9c1f2703d9d9634d81d02d
Miao Miao (苗苗) <miao.miao@spreadtrum.com>
2017-09-08 16:33:28 +0800

    Bug#744281 Manual network searching fail on vice card
    [root cause  ] Before manual network searching,Framework will
     deactivate data connection and RIL will also do this operation.In this
    situation,Framework deactivated cid=2 which was activated by Framework.RIL
    deactivated cid=1 which is default bearer.when request of manual network
    searching was send to ril,it will wait for deactivating complete, then
    AT+COPS=? could be sent to modem.It takes a bit long time(100ms) which lead to
    send lately than another stop AT command.
    [changes     ] Do not do cleanup data connection again in RIL before manual network searching
    [side effects] None
    [self test   ]:
    1.Insert two sim card in phone,make sim1 as primary and data card,sim2 as vice card
    2.Wait for sim1 data connection setup successfully
    3.Do manual network searching on sim2
    4.Stop manual network searching before search network successfully
    5.Do manual network searching again on sim2 for some times.
    6.Check the result of searching and pdp state.
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9850
    [test case]:
    1.Insert two sim card in phone,make sim1 as primary and data card,sim2 as vice card
    2.Wait for sim1 data connection setup successfully
    3.Do manual network searching on sim2
    4.Stop manual network searching before search network successfully
    5.Do manual network searching again on sim2 for some times.
    6.Check the result of searching and pdp state.
    [reviewers   ] zhiwei.fu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ic1f97b2129fa8d9990c685b2fea1c5ace4c96ff8

--- sprd-ril/ril_data.h
+++ sprd-ril/ril_data.h
@@ -122,13 +122,12 @@ typedef enum {
 } RIL_CP_DataCallFailCause;
 
 extern int s_dataAllowed[SIM_COUNT];
+extern int s_manualSearchNetworkId;
 extern bool s_LTEDetached[SIM_COUNT];
 extern struct PDPInfo s_PDP[MAX_PDP];
 
 void putPDP(int cid);
 int isExistActivePdp();
-void activeAllConnections();
-void cleanUpAllConnections();
 int processDataRequest(int request, void *data, size_t datalen, RIL_Token t,
                           int channelID);
 int processDataUnsolicited(RIL_SOCKET_ID socket_ID, const char *s);
