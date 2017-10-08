387367856939248e1895377b65343aca77cd5f01
jun.wang <jun.wang@spreadtrum.com>
2017-10-08 13:59:12 +0800

    Bug #751898 when radio off,process the last call fail cause
    [root cause ] radio off,call fail cause return error
    [changes ] when radio off,process the last call fail cause
    [side effects] no
    [self test ]:
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:9850
    [test case]:MO call and hang up
    [reviewers ] zhiwei.fu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ia048160bf38244910689d56d28234ea545cfea25

--- sprd-ril/sprd_ril.c
+++ sprd-ril/sprd_ril.c
@@ -336,6 +336,7 @@ void processRequest(int request, void *data, size_t datalen, RIL_Token t,
                  request == RIL_REQUEST_GET_NEIGHBORING_CELL_IDS ||
                  request == RIL_REQUEST_GET_MUTE ||
                  request == RIL_REQUEST_GET_CURRENT_CALLS ||
+                 request == RIL_REQUEST_LAST_CALL_FAIL_CAUSE ||
                  /* IMS Request @{ */
                  request == RIL_REQUEST_GET_IMS_CURRENT_CALLS ||
                  request == RIL_REQUEST_SET_IMS_VOICE_CALL_AVAILABILITY ||
