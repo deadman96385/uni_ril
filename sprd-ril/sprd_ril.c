b7c5ebdfd377a51d045a6c32e1f895982187c40b
lisayanan.li <lisayanan.li@spreadtrum.com>
2017-11-29 09:58:20 +0800

    Bug #627298 [7.0_17b] modify ril adapt to 4mode L+L
    [root cause  ] ril don't support 4mode L+L
    [changes     ] modify ril adapt to 4mode L+L
    [side effects]no
    [self test   ]:build successfully
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl2
    [test case]:LL case
    [reviewers   ] zhiwei.fu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: If9b970be0f44a4eab023e53852bec433d5c2b5ff

--- sprd-ril/sprd_ril.c
+++ sprd-ril/sprd_ril.c
@@ -1011,7 +1011,8 @@ int getModemConfig() {
     property_get(MODEM_CONFIG_PROP, prop, "");
     if (strcmp(prop, "TL_LF_TD_W_G,W_G") == 0 || strcmp(prop, "TL_LF_W_G,W_G") == 0) {
         modemConfig = LWG_WG;
-    } else if (strcmp(prop, "TL_LF_TD_W_G,TL_LF_TD_W_G") == 0) {
+    } else if (strcmp(prop, "TL_LF_TD_W_G,TL_LF_TD_W_G") == 0 ||
+               strcmp(prop, "TL_LF_W_G,TL_LF_W_G") == 0) {
         modemConfig = LWG_LWG;
     }
     return modemConfig;
