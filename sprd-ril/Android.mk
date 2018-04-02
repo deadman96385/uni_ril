f616af31f2cf6a0bef6bd7816189b03c6d44933f
lisayanan.li <lisayanan.li@spreadtrum.com>
2018-04-02 13:44:53 +0800

    Bug #851412 [8.1][SPCSS00463275] ril 配置单卡
    [root cause  ] 客户版本双卡，需求单卡
    [changes     ] ril 配置为单卡
    [side effects]no
    [self test   ]:build successfully
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl2
    [test case]:single sin card case
    [reviewers   ] zhiwei.fu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I2f7369a46053e598f3802f32c27587703e3f67c1

--- sprd-ril/Android.mk
+++ sprd-ril/Android.mk
@@ -37,14 +37,6 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libril_threads
 
 LOCAL_CFLAGS += -DSIM_AUTO_POWERON  -DRIL_EXTENSION
 
-ifneq ($(SIM_COUNT), 1)
-    LOCAL_CFLAGS += -DANDROID_MULTI_SIM
-endif
-
-ifeq ($(SIM_COUNT), 2)
-    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
-endif
-
 LOCAL_PROPRIETARY_MODULE := true
 
 ifeq (foo,foo)
