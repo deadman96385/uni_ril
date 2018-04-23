91f76de3035e341bd7510f8451317d0b11db68d0
Miao Miao (苗苗) <miao.miao@spreadtrum.com>
2018-04-23 10:43:08 +0800

    Bug#859162 pdp cannot active on single sim L+L version
    [root cause  ] Device could not send allow data request on single sim version witch lead to pdp could not active
    [changes     ] If device is single sim version,active pdp ignor allow data flag
    [side effects] None
    [self test   ]:Check pdp state on single sim L+L version
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9850
    [test case]:Check pdp state on single sim L+L version
    [reviewers   ] zhiwei.fu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Id5d1fc355f572bfbd1090b27968bfc65b9bc5c21

--- sprd-ril/ril_data.c
+++ sprd-ril/ril_data.c
@@ -2244,6 +2244,7 @@ int processDataRequest(int request, void *data, size_t datalen, RIL_Token t,
                     RLOGD("SETUP_DATA_CALL s_PSRegState[%d] = %d", socket_id,
                           s_PSRegState[socket_id]);
                     if (s_PSRegState[socket_id] == STATE_IN_SERVICE) {
+#if (SIM_COUNT == 2)
                         /* bug813401 L+L version,only setup data call on data card @{ */
                         if (s_modemConfig == LWG_LWG && s_dataAllowed[socket_id] != 1) {
                             s_lastPDPFailCause[socket_id] =
@@ -2253,6 +2254,7 @@ int processDataRequest(int request, void *data, size_t datalen, RIL_Token t,
                             break;
                         }
                         /* }@ */
+#endif
                         requestSetupDataCall(channelID, data, datalen, t);
                         s_failCount = 0;
                     } else {
