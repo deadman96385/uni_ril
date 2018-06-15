b027ea9aac1edb8ae45039fa4b39ebcce4a72626
rongmei.li <rongmei.li@spreadtrum.com>
2018-06-15 09:04:25 +0800

    Bug #908249 DUT removes VT upgrade button for call A-C when call A-B is put on hold from device B in mentioned scenario
    [root cause  ] listen the CSSU,set the property is not for the current call id
    [changes     ] extend the unsolicited for CSSU,add the property of call id
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9850ka
    [test case]:DUT should not remove VT upgrade request for call A-C when call A-B is put on hold from B.test pass
    [reviewers   ] lei.huang
    [change_type ] bugfix
    [tag_product ] common
    
    Change-Id: I914e42d1e1bdf2bc43f503dc1a9647d1524c81f9
    (cherry picked from commit b485a58f4339f161bfb2fca12642ac171c3a51e1)

--- sprd-ril/ril_ss.c
+++ sprd-ril/ril_ss.c
@@ -1303,6 +1303,7 @@ int processSSUnsolicited(RIL_SOCKET_ID socket_id, const char *s) {
     } else if (strStartsWith(s, "+CSSU:")) {
         RIL_SuppSvcNotification *response = NULL;
         int code = 0;
+        int index = 0;
         char *tmp;
 
         response = (RIL_SuppSvcNotification *)
@@ -1315,13 +1316,18 @@ int processSSUnsolicited(RIL_SOCKET_ID socket_id, const char *s) {
         at_tok_start(&tmp);
         err = at_tok_nextint(&tmp, &code);
         if (err < 0) {
-            RLOGD("%s fail", s);
+            RLOGD("%s code fail", s);
             free(response);
             goto out;
         }
+        err = at_tok_nextint(&tmp, &index);
+        if (err < 0) {
+            RLOGD("%s index fail ", s);
+            index = 0;
+        }
         response->notificationType = MT_CALL;
         response->code = code;
-        response->index = 0;
+        response->index = index;
         response->type = 0;
         response->number = NULL;
 
